df2f07c7b43ecdd891eec6fe6d948ccd78b267c6
Sierra MacLeod <sierra.m.macleod@gmail.com>
2020-03-13 11:25:51 -0600

    ip to localhost

--- TangoPi/__main__.py
+++ TangoPi/__main__.py
@@ -2,7 +2,7 @@ import tkinter
 from input import networkgui
 
 
-HOST = '10.200.26.39'  # Standard loopback interface address (localhost)
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 9090        # Port to listen on (non-privileged ports are > 1023)
 
 
