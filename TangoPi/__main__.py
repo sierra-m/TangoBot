5425ceaccbee67d56b310bcfaf79b11cfc27a93f
Sierra MacLeod <sierra.m.macleod@gmail.com>
2020-03-13 11:22:08 -0600

    network edits

--- TangoPi/__main__.py
+++ TangoPi/__main__.py
@@ -1,5 +1,5 @@
 import tkinter
-from input import network
+from input import networkgui
 
 
 HOST = '10.200.26.39'  # Standard loopback interface address (localhost)
@@ -7,6 +7,6 @@ PORT = 9090        # Port to listen on (non-privileged ports are > 1023)
 
 
 window = tkinter.Tk()
-input_handler = network.NetworkControl(window, HOST, PORT)
+input_handler = networkgui.NetworkControl(HOST, PORT)
 
 window.mainloop()
